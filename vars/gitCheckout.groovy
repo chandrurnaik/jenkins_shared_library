de6b26847cbd88d16b239ebe06d5fd11dace20bf
Chandru <chandrunaik051@gmail.com>
2023-10-04 19:58:29 +0530

    custome push and pull 10/04/23 19:58:27

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,9 @@
 def call(String git_branch = "main", String git_crds = null, String git_url = null) {
+     echo "Starting SCM Checkout gitCheckout.groovy"
      checkout([
         $class: 'GitSCM', 
         branches: [[name: "*/${git_branch}"]], 
         userRemoteConfigs: [[credentialsId: "${git_crds}", url: "${git_url}"]]
     ])
+    sh 'pwd; ls -lrt'
 }
