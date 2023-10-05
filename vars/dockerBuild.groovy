d0cd3e7ce8914be2d529ae27e27a56eba6caf745
Chandru <chandrunaik051@gmail.com>
2023-10-05 09:15:43 +0530

    git checkout complete 10/05/23 09:15:42

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
-def call(String dockerfile = null,
+def call(String dockerfile = 'Dockerfile',
          String docker_context = null,
          String image_name = null,
          String image_tag = null,
@@ -7,7 +7,7 @@ def call(String dockerfile = null,
     def customImage = docker.build("${image_name}:${image_tag}",
                                    "-f ${dockerfile} ${docker_context}")
     docker.withRegistry("${registry_url}", "${registry_crds}") {
-        
+
         customImage.push()
     }
 }
